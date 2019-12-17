ad33c972dcafe145c3248d6b8175d45dc80373da
thomas dubiel <td1729@hotmail.com>
2019-12-17 11:09:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Capulet" 
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
@@ -32,6 +32,9 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
